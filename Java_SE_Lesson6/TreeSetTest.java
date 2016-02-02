f5912571d04518b812ccc4a914644ad4a47cef5b
stacktrace828 <stacktrace828@gmail.com>
2016-02-02 22:25:05 +0800

    review lesson6

---
+++ Java_SE_Lesson6/TreeSetTest.java
@@ -0,0 +1,20 @@
+package com.stacktrace;
+import java.util.TreeSet;
+
+public class TreeSetTest
+{
+	public static void main(String[] args)
+	{
+		TreeSet set = new TreeSet();
+		
+		set.add("C");
+		set.add("A");
+		set.add("B");
+		set.add("E");
+		set.add("F");
+		set.add("D");
+		
+		System.out.println(set);
+		
+	}
+}
